919d2207e494a1bbb87b8001ef013b6b0db15d94
devCTS <dev@catalyst.sh>
2025-06-18 23:03:42 +0530

    Added course entity service,controller and module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentController } from './student/student.controller';
+import { StudentService } from './student/student.service';
+import { StudentModule } from './student/student.module';
+import { CourseController } from './course/course.controller';
+import { CourseModule } from './course/course.module';
+import { TeacherService } from './teacher/teacher.service';
+import { TeacherController } from './teacher/teacher.controller';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [StudentModule, CourseModule, TeacherModule],
+  controllers: [AppController, StudentController, CourseController, TeacherController],
+  providers: [AppService, StudentService, TeacherService],
 })
 export class AppModule {}
