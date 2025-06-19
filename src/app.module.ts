945bf1b3b3ba9563943e4f6c5408abacd6d16e81
devCTS <dev@catalyst.sh>
2025-06-19 11:54:50 +0530

    Added url for mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StudentController } from './student/student.controller';
-import { StudentService } from './student/student.service';
 import { StudentModule } from './student/student.module';
-import { CourseController } from './course/course.controller';
 import { CourseModule } from './course/course.module';
-import { TeacherService } from './teacher/teacher.service';
-import { TeacherController } from './teacher/teacher.controller';
 import { TeacherModule } from './teacher/teacher.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [StudentModule, CourseModule, TeacherModule],
-  controllers: [AppController, StudentController, CourseController, TeacherController],
-  providers: [AppService, StudentService, TeacherService],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest-pagination-task1'), StudentModule, CourseModule, TeacherModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
