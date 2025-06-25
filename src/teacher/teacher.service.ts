f1bb5486390c049fb89392b3cc608d36202653b8
devCTS <dev@catalyst.sh>
2025-06-25 17:10:22 +0530

    feat: add create update get and delete function for teacher entity in teacher service

--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from "@nestjs/mongoose";
+import { Teacher } from "./schema/teacher.schema";
+import { Model } from "mongoose";
+import { TeacherRequestDto } from "./dto/create-teacher.dto";
 
 @Injectable()
-export class TeacherService {}
+export class TeacherService {
+  constructor(
+    @InjectModel(Teacher.name) private readonly teacherModel: Model<Teacher>,
+  ) {}
+
+  async createTeacher(teacherRequestData: TeacherRequestDto): Promise<Teacher> {
+    const newTeacherData = new this.teacherModel(teacherRequestData);
+    return await newTeacherData.save();
+  }
+
+  async findAllTeachers(): Promise<Teacher[]> {
+    return await this.teacherModel.find().lean();
+  }
+
+  async findTeacher(id: string): Promise<Teacher> {
+    const teacherData = await this.teacherModel.findById(id).lean();
+    if (!teacherData) throw new NotFoundException(`Teacher with ${id} is not found`);
+    return teacherData;
+  }
+
+  async updateTeacher(id: string, teacherRequestData: TeacherRequestDto): Promise<Teacher> {
+    const teacherUpdatedData = await this.teacherModel
+      .findByIdAndUpdate(id, teacherRequestData, {
+        new: true,
+        runValidators: true,
+      })
+      .exec();
+    if (!teacherUpdatedData) throw new NotFoundException(`Teacher with id ${id} is not found`);
+    return teacherUpdatedData;
+  }
+
+  async deleteTeacher(id: string): Promise<void> {
+    const deletedTeacher = await this.teacherModel.findByIdAndDelete(id).lean();
+    if (!deletedTeacher) throw new NotFoundException(`Teacher with id ${id} is not found`);
+  }
+}
