ae154f90be316c02d2be4342be3c8f6787914e76
devCTS <dev@catalyst.sh>
2025-06-25 16:57:28 +0530

    chore: add ConfigModule for environment variable support in app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { CourseModule } from './course/course.module';
 import { TeacherModule } from './teacher/teacher.module';
-import { MongooseModule } from "@nestjs/mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URL as string),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL!),
     StudentModule,
     CourseModule,
     TeacherModule,
