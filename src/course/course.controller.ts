88b61d83331e9ab223f72db163b10211ace45fa7
devCTS <dev@catalyst.sh>
2025-07-01 16:09:41 +0530

    feature: Added crud operations for course entity

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CourseService } from './course.service';
+import { CourseRequestDto } from './dto/create-course.dto';
+import { CourseResponseDto } from './dto/course-response.dto';
+import { transformResponse } from "src/utils/common/common-functions";
 
 @Controller('course')
-export class CourseController {}
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Post()
+  async createCourse(@Body() courseRequestData: CourseRequestDto) {
+    const courseData = await this.courseService.createCourse(courseRequestData);
+    return transformResponse(CourseResponseDto,courseData)
+  }
+
+  @Get()
+  async findAllCourses() {
+    const coursesData = await this.courseService.findAllCourses();
+    return transformResponse(CourseResponseDto, coursesData)
+  }
+
+  @Get(':id')
+  async findCourse(@Param('id') id: string) {
+    const courseData = this.courseService.findCourseById(id);
+    return transformResponse(CourseResponseDto, courseData)
+  }
+
+  @Put(':id')
+  async updateCourse(
+    @Param('id') id: string,
+    @Body() courseRequestData: CourseRequestDto,
+  ) {
+    const updatedCourseData = this.courseService.updateCourse(id, courseRequestData);
+    return transformResponse(CourseResponseDto, updatedCourseData)
+  }
+
+  @Delete(':id')
+  async deleteCourse(@Param('id') id: string) {
+    await this.courseService.deleteCourse(id);
+    return { message: 'Course deleted successfully' };
+  }
+}
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Course } from './schema/course.schema';
+import { CourseRequestDto } from './dto/create-course.dto';
+import { checkNotFoundError } from 'src/utils/common/common-functions';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(
+    @InjectModel(Course.name) private readonly courseModel: Model<Course>,
+  ) {}
+
+  async createCourse(courseRequestData: CourseRequestDto): Promise<Course> {
+    const createdCourseData = new this.courseModel(courseRequestData);
+    return createdCourseData.save();
+  }
+
+  async findAllCourses(): Promise<Course[]> {
+    return this.courseModel.find().populate('teacher').lean();
+  }
+
+  async findCourseById(id: string): Promise<Course> {
+    const courseData = await this.courseModel
+      .findById(id)
+      .populate('teacher')
+      .lean();
+    checkNotFoundError(courseData, 'Course', id);
+    return courseData as Course;
+  }
+
+  async updateCourse(id: string, courseRequestData: CourseRequestDto): Promise<Course> {
+    const updatedCourseData = await this.courseModel
+      .findByIdAndUpdate(id, courseRequestData, { new: true, runValidators: true })
+      .populate('teacher')
+      .lean();
+    checkNotFoundError(updatedCourseData,'Course',id)
+    return updatedCourseData as Course;
+  }
+
+  async deleteCourse(id: string): Promise<void> {
+    const deletedCourseData = await this.courseModel.findByIdAndDelete(id).lean();
+    checkNotFoundError(deletedCourseData,'Course',id)
+  }
+}
